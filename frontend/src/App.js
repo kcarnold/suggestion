29e5d778182e43ce077dd82a5541c8926e7c4040
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2016-12-13 16:16:58 -0500

    Pull in the keyboard.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
+
+var KEYLABELS = {
+    ' ': 'space'
+//      '⏎': 'Done'
+};
+
+class Keyboard extends Component {
+  render() {
+    return <div className="Keyboard">{
+      ['qwertyuiop', 'asdfghjkl', '\'?zxcvbnm⌫', '-!, .⏎'].map(function(row, i) {
+          return <div key={i} className="row">{
+            _.map(row, function(key, j) {
+              // if (layer === 'upper') key = key.toUpperCase();
+              var label = key in KEYLABELS ? KEYLABELS[key] : key;
+              var className = 'key';
+              if ('⏎⌫\'-!,.?'.indexOf(key) !== -1) className += ' key-reverse';
+              return <div key={key} className={className} data-key={key}>{label}</div>;
+          })}</div>
+          })}
+      </div>;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Keyboard />
       </div>
     );
   }
