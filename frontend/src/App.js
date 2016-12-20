b6e1bc4603b99951f808f5438a99aeba3ae9370f
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2016-12-20 16:58:43 -0500

    WIP suggestion state management.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 import _ from 'lodash';
-import {extendObservable, autorun} from 'mobx';
+import M from 'mobx';
 // unused: observable, action, toJS, transaction
 import {observer, inject, Provider} from 'mobx-react';
 import WSClient from './wsclient';
@@ -22,7 +20,18 @@ window.localStorage['clientId'] = clientId;
 ws.sendHello({type: 'init', participantId: clientId});
 
 
+/**** Event dispatching
 
+This is how we split the difference between Flux everything-is-a-big-global-action and mobx just-mutate-stuff:
+All input comes in as events represented as plain JSON objects. The level of interpretation should be pragmatic:
+low-level enough to be able to get fine-grained detail about what happened, but high-level enough to be able
+to read off interesting things without much work. e.g., for a key tap, include the tap x/y position, but also
+what key we thought it was.
+
+All server communication comes in this way too.
+ */
+
+// Dispatch by type. This was probably premature...
 var handlersByType = {};
 
 function registerHandler(eventType, fn) {
@@ -32,6 +41,7 @@ function registerHandler(eventType, fn) {
 
 function dispatch(event) {
   console.log(event);
+  log(event);
   event.timestamp = +new Date();
   let handlers = handlersByType[event.type];
   if (!handlers) {
@@ -39,20 +49,114 @@ function dispatch(event) {
   } else {
     handlers.forEach(fn => fn(event));
   }
-  log(event);
 }
 
+// Every event gets logged to the server. Keep events small!
 function log(event) {
   ws.send({type: 'log', event});
 }
 
 
+/**** Main state store!
+
+This represents all the state, UI state included. The entire UI should be a function of this, with perhaps a few pragmatic exceptions (none yet - 2016-12-17).
+
+A Suggestion is an important object. It contains:
+{
+  // The context in which it was made
+  contextSequenceNum: int
+  prefix: string : characters before this suggestion
+  tapLocations: array of {x: int, y: int}: cur-word taps
+  words: array of string: the words of the suggestion
+  probs: array of float: the natural-log generation probabilities of each word
+  isValid: for a visible suggestion, whether it's valid in the current suggestion context.
+}
+
+We keep a list of the current suggestions. If a suggestion's sequence number doesn't match, we can still show it (so as to not be jarring), but dim it, and don't allow acceptance.
+
+After tapping a suggestion, it becomes *active*, which means that if we keep tapping on that same spot, subsequent words will get inserted. To do this, we keep a notion of an `activeSuggestion`, which is separate from the suggestions returned from the server:
+{
+  suggestion: the Suggestion object
+  slot: the slot number where it lives
+  wordIdx: the index of the next word to insert.
+}
+
+We're not caching because (1) caching is a Hard Problem and (2) key taps will be at different places anyway, so caching won't really help.
+It may be nice to prefetch what happens when a suggestion gets pressed, because there are only 3 states there and we know them exactly.
+But TODO.
+
+How this gets updated:
+An autorun thread watches contextSequenceNum and requests suggestions when the context changes.
+
+visibleSuggestions is a pure computation based on the last suggestions received from the server, the current context sequence number, and the active suggestion. It puts them all together, keeping track of what's valid.
+
+*/
 class StateStore {
   constructor() {
     this.__version__ = 1;
-    extendObservable(this, {
-      curText: 'Test text',
+    M.extendObservable(this, {
+      curText: '',
       tapLocations: [],
+      contextSequenceNum: 0,
+      lastSuggestionsFromServer: null,
+      activeSuggestion: null,
+      get activeSuggestionWords() {
+        return this.activeSuggestion.suggestion.words.slice(this.activeSuggestion.wordIdx);
+      },
+      get visibleSuggestions() {
+        // The visible suggestions are:
+        // - The active ("promised") suggestion, in its corresponding slot.
+        // - Suggestions from the server, filled in as long as they're valid.
+
+        // Copy the suggestions so we can tweak them.
+        let suggestions = M.toJS(this.lastSuggestionsFromServer);
+
+        if (this.activeSuggestion) {
+          suggestions[this.activeSuggestion.slot] = {
+            orig: this.activeSuggestion.suggestion,
+            contextSequenceNum: this.contextSequenceNum,
+            words: this.activeSuggestionWords,
+          }
+        }
+        return suggestions;
+      },
+      tapKey: M.action(event => {
+        this.curText += event.key;
+        this.tapLocations.push({x: event.x, y: event.y});
+        this.contextSequenceNum++;
+      }),
+      tapBackspace: M.action(() => {
+        this.curText = this.curText.slice(0, -1);
+        this.tapLocations.pop();
+        this.contextSequenceNum++;
+      }),
+      insertSuggestion: M.action(slot => {
+        let wordToInsert = null;
+        let tappedSuggestion = this.visibleSuggestions[slot];
+        if (this.activeSuggestion !== null && this.activeSuggestion.slot === slot) {
+          // Continuing a previous suggestion.
+          wordToInsert = this.activeSuggestionWords[0];
+          if (this.activeSuggestionWords.length > 1) {
+            // Words remain, advance the active suggestion.
+            this.activeSuggestion.wordIdx++;
+          } else {
+            // No more words in the active suggestion, go back to normal.
+            this.activeSuggestion = null;
+          }
+        } else if (tappedSuggestion.contextSequenceNum === this.contextSequenceNum) {
+          wordToInsert = tappedSuggestion.words[0];
+          if (tappedSuggestion.words.length > 1) {
+            this.activeSuggestion = {
+              suggestion: tappedSuggestion,
+              slot: slot,
+              wordIdx: 1
+            };
+          }
+        }
+        let {prefix} = this.getSuggestionContext();
+        this.curText = prefix + wordToInsert;
+        this.tapLocations = this.tapLocations.slice(0, prefix.length).concat(wordToInsert.map(() => null));
+      }),
     });
   }
 
@@ -75,28 +179,19 @@ class StateStore {
 }
 
 var state = new StateStore();
-window.state = state;
 
-var suggestionCache = {};
-
-var curRequestId = 0;
-var requestsById = {};
-
-autorun(() => {
-  if (!(state.curText in suggestionCache)) {
-    // FIXME: this ignores sub-key taps.
-    let context = state.getSuggestionContext()
-    let {prefix, curWord} = context;
-    ws.send({
-      type: 'requestSuggestions',
-      request_id: curRequestId,
-      sofar: prefix,
-      cur_word: curWord,
-      temperature: .5,
-      domain: 'yelp_train',
-    });
-    requestsById[curRequestId++] = context;
-  }
+// Auto-runner to watch the context and request suggestions.
+M.autorun(() => {
+  let context = M.untracked(() => state.getSuggestionContext());
+  let {prefix, curWord} = context;
+  ws.send({
+    type: 'requestSuggestions',
+    request_id: state.contextSequenceNum,
+    sofar: prefix,
+    cur_word: curWord,
+    temperature: .5,
+    domain: 'yelp_train',
+  });
 });
 
 ws.onmessage = function(msg) {
@@ -107,22 +202,41 @@ ws.onmessage = function(msg) {
 
 
 registerHandler('tapKey', event => {
-  state.curText += event.key;
+  state.tapKey(event);
 });
 
 registerHandler('tapBackspace', event => {
-  state.curText = state.curText.slice(0, -1);
+  state.tapBackspace();
 });
 
 registerHandler('receivedSuggestions', ({msg}) => {
-  let {request_id} = msg;
-  if (!(request_id in requestsById)) {
-    console.warn("Got a response to a request we didn't make??");
+  // Only update suggestions if the data is valid.
+  if (msg.request_id !== state.contextSequenceNum) {
+    console.log("Discarding outdated suggestions", msg.request_id, state.contextSequenceNum);
     return;
   }
-  let {prefix, curWord} = requestsById[request_id];
-  delete requestsById[request_id];
-  console.log(prefix, curWord, msg);
+
+  let suggestions = msg.next_word.map(sugg => ({
+    orig: sugg,
+    contextSequenceNum: msg.request_id,
+    words: sugg.one_word.words.concat(sugg.continuation.length ? sugg.continuation[0].words : []),
+    probs: sugg.probs,
+  }));
+
+  // Reorder the suggestions to match the active suggestion, if applicable.
+  if (state.activeSuggestion !== null) {
+    let activeSuggestionNextWord = state.activeSuggestionWords[0];
+    let nextWordIdx = _.map(suggestions, sugg => sugg.words[0]).indexOf(activeSuggestionNextWord);
+    if (nextWordIdx !== -1) {
+      // Move the corresponding server suggestion to the correct spot.
+      suggestions.splice(state.activeSuggestion.slot, 0, suggestions.splice(nextWordIdx, 1)[0]);
+    } else {
+      // not one of the next words. Leave a blank for it.
+      suggestions.splice(state.activeSuggestion.slot, 0, null);
+    }
+  }
+
+  state.lastSuggestionsFromServer = suggestions;
 });
 
 var KEYLABELS = {
@@ -168,7 +282,7 @@ class Keyboard extends Component {
     if (key === '⌫') {
       dispatch({type: 'tapBackspace'});
     } else {
-      dispatch({type: 'tapKey', key});
+      dispatch({type: 'tapKey', key, x: evt.clientX, y: evt.clientY});
     }
   };
 
@@ -242,3 +356,7 @@ class App extends Component {
 }
 
 export default App;
+
+// Globals
+window.M = M;
+window.state = state;
