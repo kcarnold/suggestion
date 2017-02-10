603c45b9f35a42fbd462162dc66f021ca439ed07
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-02-10 14:56:59 -0500

    Clean up the code a bit and show a demo with low temp

--- tmp/retrieval_model.py
+++ tmp/retrieval_model.py
@@ -14,23 +14,16 @@ import kenlm
 docs = json.load(open('models/tokenized_reviews.json'))
 #%%
 sufarr = suffix_array.DocSuffixArray(docs=docs, **joblib.load('models/yelp_sufarr.joblib'))
-#sufarr = suffix_array.DocSuffixArray(docs=docs, **joblib.load('../megacomplete/yelp_sufarr.joblib'))
 #%%
 from suggestion import suggestion_generator
 model = suggestion_generator.get_model('yelp_train')
 #%%
-
-context = 'my friends '
-context_toks = suggestion_generator.tokenize_sofar(context)
-#%%
-a, b = sufarr.search_range((context_toks[-1], ''))
 def collect_words_in_range_slow(start, after_end, word_idx):
     words = set()
     for idx in range(start, after_end):
         words.add(sufarr.get_suffix_by_idx(idx)[word_idx])
     return words
-set_a = collect_words_in_range_slow(a, b, 1)
-#%%
+
 def collect_words_in_range(start, after_end, word_idx):
     words = set()
     if start == after_end:
@@ -45,27 +38,30 @@ def collect_words_in_range(start, after_end, word_idx):
             assert word not in words
             words.add(word)
     return words
-#set_b = collect_words_in_range(a, b, 1)
-
-end = b
-assert collect_words_in_range_slow(a, end, 1) == collect_words_in_range(a, end, 1)
+#assert collect_words_in_range_slow(a, end, 1) == collect_words_in_range(a, end, 1)
 #%%
+import scipy.stats
 def generate_phrase_from_sufarr(model, sufarr, context_toks, length, temperature=1.):
     if context_toks[0] == '<s>':
         state, _ = model.get_state(context_toks[1:], bos=True)
     else:
         state, _ = model.get_state(context_toks, bos=False)
-    start_idx, end_idx = sufarr.search_range((context_toks[-1], ''))
     phrase = []
     generated_logprobs = np.empty(length)
     for i in range(length):
+        start_idx, end_idx = sufarr.search_range((context_toks[-1],) + tuple(phrase) + ('',))
         next_words = sorted(collect_words_in_range(start_idx, end_idx, i + 1))
         if len(next_words) == 0:
             raise suggestion_generator.GenerationFailedException
+#        if len(next_words) == 1:
+            # We
         vocab_indices = [model.model.vocab_index(word) for word in next_words]
         logprobs = model.eval_logprobs_for_words(state, vocab_indices)
         logprobs /= temperature
         probs = suggestion_generator.softmax(logprobs)
+        if len(next_words) < 10:
+            print(next_words, probs)
+        print(start_idx, end_idx-start_idx, len(next_words), scipy.stats.entropy(probs))
 
         picked_subidx = np.random.choice(len(probs), p=probs)
         picked_idx = vocab_indices[picked_subidx]
@@ -76,9 +72,10 @@ def generate_phrase_from_sufarr(model, sufarr, context_toks, length, temperature
 #        assert word == model.id2str[picked_idx]
         phrase.append(word)
         generated_logprobs[i] = np.log(probs[picked_subidx])
-        start_idx, end_idx = sufarr.search_range((context_toks[-1],) + tuple(phrase) + ('',))
-        print(start_idx, end_idx-start_idx)
     return phrase, generated_logprobs
 
-generate_phrase_from_sufarr(model, sufarr, context_toks, 30)
+
+context = 'my '
+context_toks = suggestion_generator.tokenize_sofar(context)
+generate_phrase_from_sufarr(model, sufarr, context_toks, 30, temperature=.01)
 #%%
