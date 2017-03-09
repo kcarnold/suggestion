0707899b19d4941af50cd4c17ad836c98b54c620
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-03-09 18:10:40 -0500

    Add a way to show all the screens in the experiment quickly.

---
+++ frontend/src/ShowAllScreens.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import M from 'mobx';
+import {observer, Provider} from 'mobx-react';
+import {MasterStateStore} from './MasterStateStore';
+import {MasterView} from './Views';
+
+let states = [new MasterStateStore('zzzzzz')];
+let screens = states[0].screens;
+states[0].replaying = false;
+for (let i=1; i<screens.length; i++) {
+  let newState = new MasterStateStore('zzzzzz');
+  for (let j=0; j<i; j++) {
+    newState.handleEvent({type: 'next'});
+  }
+  states.push(newState);
+  newState.replaying = false;
+}
+
+const ShowAllScreens = observer(class ShowAllScreens extends Component {
+  render() {
+    return <div>
+      {states.map((state, i) => <div key={i} style={{display: 'flex', flewFlow: 'row'}}>
+        <div style={{overflow: 'hidden', width: 360, height: 599, border: '1px solid black'}}>
+          <Provider state={state} dispatch={() => {}} clientId={'zzzzzz'} clientKind={'p'} spying={true}>
+            <MasterView kind={'p'}/>
+          </Provider>
+        </div>
+        <div style={{overflow: 'hidden', width: 500, height: 700, border: '1px solid black'}}>
+          <Provider state={state} dispatch={() => {}} clientId={'zzzzzz'} clientKind={'c'} spying={true}>
+            <MasterView kind={'c'} />
+          </Provider>
+        </div>
+      </div>)
+    }</div>;
+  }
+});
+
+export default ShowAllScreens;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-let isPanopticon = window.location.search.slice(1, 7) === 'panopt';
 let topLevel;
-if (isPanopticon) {
+if (window.location.search.slice(1, 7) === 'panopt') {
   let Panopticon = require('./Panopticon').default;
   topLevel = <Panopticon />;
+} else if (window.location.search.slice(1) === 'showall') {
+  let ShowAllScreens = require('./ShowAllScreens').default;
+  topLevel = <ShowAllScreens />;
 } else {
   let App = require('./App').default;
   topLevel = <App />;
