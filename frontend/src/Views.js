8d64ca4b8a2505b038c7eb7df6cbed54311f3675
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-03-11 18:46:55 -0500

    More options for pairing the phone

--- frontend/src/Views.js
+++ frontend/src/Views.js
@@ -274,8 +274,13 @@ export const screenViews = {
   SetupPairingComputer: inject('clientId')(({clientId}) => <div>
     <p>You will need two devices to complete this study: a <b>laptop/desktop computer</b> (you could use a tablet but we haven't tested it), and a <b>smartphone</b> with a web browser and WiFi (we will not be responsible for any data charges).</p>
 
-    <p>On your phone's web browser, go to <tt>megacomplete.net</tt> and enter <tt>{clientId}-p</tt>. There will be a button on that page to continue.</p>
-    <p>If you have a barcode reader on your phone, you can use scan this:<br/><img src={"https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=" + encodeURIComponent("http://megacomplete.net/?" + clientId + "-p")} role="presentation"/></p>
+    <div>How to pair your phone (they're all the same, pick the easiest one for you):</div>
+    <ul>
+      <li>On your phone's web browser, go to <tt>megacomplete.net</tt> and enter <tt>{clientId}-p</tt>.</li>
+      <li>Send this link to yourself: <input style={{fontFamily: 'monospace', width: '25em'}} value={`http://megacomplete.net/${clientId}-p`} /></li>
+      <li>Scan this:<br/><img src={"https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=" + encodeURIComponent("http://megacomplete.net/?" + clientId + "-p")} role="presentation"/></li>
+    </ul>
+    <p>Once your phone is paired, there will be a button on that page to continue.</p>
   </div>),
   SetupPairingPhone: () => <div>Successfully paired! <NextBtn /></div>,
 
