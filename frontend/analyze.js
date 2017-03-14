7cfe1642d64bdb95baeb2b0d0f85348d35885f0b
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-03-14 17:05:08 -0400

    Update analyzer to new state management format, simplifies it.

--- frontend/analyze.js
+++ frontend/analyze.js
@@ -14,46 +14,40 @@ function readStdin(callback) {
 }
 
 readStdin(function(err, res) {
+  var participants = new Map();
   var log = res.split('\n').filter(line => line.length > 0).map(line => JSON.parse(line));
-  var state = new MasterStateStore();
-  var annotated = [];
-  var requests = [];
-  var lastText = (state.experimentState || {}).curText;
-  var curTrial = {};
-  var trials = [];
-  var prevScreen = null;
-  let pageDurs = [];
-  let pageStartTime = null;
   log.forEach((entry) => {
+    if (entry.kind === 'meta') return;
+    let {participant_id} = entry;
+    let experiment = participants.get(participant_id);
+    if (!experiment) {
+      experiment = {
+        state: new MasterStateStore(),
+        annotated: [],
+        requests: [],
+      };
+      participants.set(participant_id, experiment);
+    }
+    let {state, annotated, requests} = experiment;
+    let lastText = (state.experimentState || {}).curText;
     state.handleEvent(entry);
     if (state.experimentState && state.experimentState.curText !== lastText) {
       annotated.push({...entry, curText: state.experimentState.curText});
-      requests.push(state.suggestionRequest);
+      requests.push({...state.suggestionRequest, page: state.curExperiment});
       lastText = state.curText;
     }
-    let curScreen = state.curScreen;
-    if (curScreen !== prevScreen) {
-      // Page transition.
-      // - record page durations
-      if (pageStartTime !== null) {
-        pageDurs.push([prevScreen.screen || prevScreen.controllerScreen, (entry.jsTimestamp - pageStartTime) / 1000]);
-      }
-      pageStartTime = entry.jsTimestamp;
-
-      // - record writing outcomes.
-      if (curScreen.controllerScreen === 'PostTaskSurvey') {
-        // Record the trial data.
-        curTrial.block = state.block;
-        curTrial.conditionName = state.conditionName;
-        let condition = state.condition;
-        curTrial.showPhrase = condition.showPhrase;
-        curTrial.prewriteText = state.experimentState.curText;
-        curTrial.editedText = state.curEditText;
-        trials.push(curTrial);
-        curTrial = {};
-      }
-      prevScreen = curScreen;
-    }
   });
-  process.stdout.write(JSON.stringify({annotated, requests, trials, pageDurs}));
+  // Summarize the sub-experiments.
+  participants = [...participants].map(([participant_id, {state, annotated, requests}]) => ([participant_id, {
+    annotated, requests,
+    screenTimes: state.screenTimes,
+    conditions: state.conditions,
+    blocks: [0, 1].map(block => ({
+      condition: state.conditions[block],
+      prewriteText: state.experiments.get(`pre-${block}`).curText,
+      finalText: state.experiments.get(`final-${block}`).curText,
+    }))
+  }]));
+
+  process.stdout.write(JSON.stringify(participants));
 });
