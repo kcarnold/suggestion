f2d12de8b20f30c9a771e6c2f59afba7cc69dc94
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-03-17 15:15:36 -0400

    Plot the distributions of word frequencies between best and rest.

--- tmp/word_freq_analysis.py
+++ tmp/word_freq_analysis.py
@@ -4,33 +4,17 @@ Created on Fri Mar 17 13:32:52 2017
 
 @author: kcarnold
 """
-import json
+import pandas as pd
 import pickle
 import numpy as np
 #%% Load all the reviews.
-restaurant_reviews = json.load(open('models/reviews.json'))
-tokenized_reviews = pickle.load(open('models/tokenized_reviews.pkl','rb'))
+data = pickle.load(open('yelp_preproc/all_data.pkl','rb'))
+vocab, freqs = data['vocab']
+reviews = data['data'].reset_index(drop=True)
+del data
 #%%
-assert len(restaurant_reviews) == len(tokenized_reviews)
-#%%
-#from collections import Counter
-import cytoolz
-word_counts = cytoolz.frequencies(cytoolz.concat(tokenized_reviews))
-#%%
-normal_word_counts = cytoolz.keyfilter(lambda k: k[0] not in '<,!.?\'-"', word_counts)
-normal_word_counts = cytoolz.valfilter(lambda v: v > 1, normal_word_counts)
-#%%
-import operator
-vocab, counts = zip(*sorted(normal_word_counts.items(), key=operator.itemgetter(1), reverse=True))
-vocab = list(vocab)
-counts = np.array(counts)
-freqs = counts / counts.sum()
 word2idx = {word: idx for idx, word in enumerate(vocab)}
 log_freqs = np.log(freqs)
-#%%
-def split_sents(tokenized):
-    return [x.split() for x in ' '.join(tokenized).replace('<D> ', '').replace('<P> ', '').replace('<S> ','').split('</S>') if x]
-doc_sentences = [split_sents(r) for r in tokenized_reviews]
 #%%
 def lookup_indices(sent):
     tmp = (word2idx.get(word) for word in sent)
@@ -42,16 +26,28 @@ def mean_log_freq(indices):
 def min_log_freq(indices):
     return np.min(log_freqs[indices]) if len(indices) else None
 
-doc_sentence_indices = [[lookup_indices(sent) for sent in doc] for doc in doc_sentences]
+doc_sentence_indices = [[lookup_indices(sent.split()) for sent in doc.split('\n')] for doc in reviews.tokenized]
 #%%
-mean_llk = [[mean_log_freq(indices) for indices in doc_indices] for doc_indices in doc_sentence_indices]
-min_llk = [[min_log_freq(indices) for indices in doc_indices] for doc_indices in doc_sentence_indices]
+import cytoolz
+mean_llk = [list(cytoolz.filter(None, [mean_log_freq(indices) for indices in doc_indices])) for doc_indices in doc_sentence_indices]
+min_llk = [list(cytoolz.filter(None, [min_log_freq(indices) for indices in doc_indices])) for doc_indices in doc_sentence_indices]
 #%%
+mean_mean_llk = pd.Series([np.mean(llks) if len(llks) > 0 else None for llks in mean_llk])
+mean_min_llk = pd.Series([np.mean(llks) if len(llks) > 0 else None for llks in min_llk])
+
+#%% Identify the best reviews.
 # Mark the top reviews: top-5 ranked reviews of restaurants with at least the median # reviews,
 # as long as they have >= 10 votes.
-
-yelp_reviews['total_votes'] = yelp_reviews['votes_cool'] + yelp_reviews['votes_funny'] + yelp_reviews['votes_useful']
-yelp_reviews['total_votes_rank'] = yelp_reviews.groupby('business_id').total_votes.rank(ascending=False)
-business_review_counts = yelp_reviews.groupby('business_id').review_count.mean()
+reviews['total_votes'] = reviews['votes_cool'] + reviews['votes_funny'] + reviews['votes_useful']
+reviews['total_votes_rank'] = reviews.groupby('business_id').total_votes.rank(ascending=False)
+business_review_counts = reviews.groupby('business_id').review_count.mean()
 median_review_count = np.median(business_review_counts)
-yelp_is_best = (yelp_reviews.review_count >= median_review_count) & (yelp_reviews.total_votes >= 10) & (yelp_reviews.total_votes_rank <= 5)
+yelp_is_best = (reviews.review_count >= median_review_count) & (reviews.total_votes >= 10) & (reviews.total_votes_rank <= 5)
+
+#%%
+import seaborn as sns
+import matplotlib.pyplot as plt
+to_plot = mean_min_llk.dropna()
+clip = np.percentile(to_plot, [2.5, 97.5])
+sns.kdeplot(to_plot[yelp_is_best], clip=clip, label='Yelp best')
+sns.kdeplot(to_plot[~yelp_is_best].dropna(), clip=clip, label='Yelp rest')
