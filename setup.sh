9598242d7b0e2acca76cc20e2d8b423d87e26f0e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-03-22 22:10:29 +0000

    Add tqdm as a dep.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-conda install -y tornado ujson cython numpy datrie nltk scipy numba scikit-learn cytoolz attrs
+conda install -y tornado ujson cython numpy datrie nltk scipy numba scikit-learn cytoolz attrs tqdm
 pip install -r requirements.txt
