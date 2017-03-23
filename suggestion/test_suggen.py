d2e299484af77e0b5e4013017692b3d8cfdd5a17
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-03-23 11:39:45 -0400

    Fix tests.

--- suggestion/test_suggen.py
+++ suggestion/test_suggen.py
@@ -10,13 +10,13 @@ def collect_words_in_range_slow(start, after_end, word_idx):
 def test_collect_words_in_range():
     sufarr = suggestion_generator.sufarr
     start, end = sufarr.search_range(tuple('<D> my'.split()) + ('',))
-    assert [] == suggestion_generator.collect_words_in_range(start, start, 1)
+    assert [] == suggestion_generator.collect_words_in_range(start, start, 1, sufarr.docs)
 
     ref = sorted(collect_words_in_range_slow(start, end, 1))
-    assert ref == suggestion_generator.collect_words_in_range(start, end, 1)
+    assert ref == suggestion_generator.collect_words_in_range(start, end, 1, sufarr.docs)
 
     ref = sorted(collect_words_in_range_slow(start, end, 2))
-    assert ref == suggestion_generator.collect_words_in_range(start, end, 2)
+    assert ref == suggestion_generator.collect_words_in_range(start, end, 2, sufarr.docs)
 
     import random
     rng = random.Random(0)
@@ -26,20 +26,20 @@ def test_collect_words_in_range():
 
         start, end = sufarr.search_range(tuple(suf) + ('',))
         ref = sorted(collect_words_in_range_slow(start, end, word_idx))
-        assert ref == suggestion_generator.collect_words_in_range(start, end, word_idx)
+        assert ref == suggestion_generator.collect_words_in_range(start, end, word_idx, sufarr.docs)
 
 
 
 def test_get_suggestions():
     # A smoke test.
     configs = [
-        dict(rare_word_bonus=1.0, use_sufarr=True, temperature=0.),
-        dict(rare_word_bonus=0.0, use_sufarr=True, temperature=0.),
-        dict(rare_word_bonus=None, use_sufarr=False, temperature=0.),
-        #dict(rare_word_bonus=None, use_sufarr=False, temperature=.5)
+        dict(use_bos_suggs=False, rare_word_bonus=1.0, use_sufarr=True, temperature=0.),
+        dict(use_bos_suggs=False, rare_word_bonus=0.0, use_sufarr=True, temperature=0.),
+        dict(use_bos_suggs=False, rare_word_bonus=None, use_sufarr=False, temperature=0.),
+        #dict(use_bos_suggs=False, rare_word_bonus=None, use_sufarr=False, temperature=.5)
         ]
     for config in configs:
-        result = suggestion_generator.get_suggestions(
+        result, new_sug_state = suggestion_generator.get_suggestions(
             sofar="one day , ",
             cur_word=[],
             domain='yelp_train',
@@ -52,9 +52,10 @@ def test_get_suggestions():
 
 def test_curword_with_no_followers():
     suggestion_generator.get_suggestions(
-        'some other foods since the ',
-        [{'letter': let} for let in 'servic'],
+        sofar='some other foods since the ',
+        cur_word=[{'letter': let} for let in 'servic'],
         domain='yelp_train',
         rare_word_bonus=1.0,
         use_sufarr=False,
-        temperature=0.)
+        temperature=0.,
+        use_bos_suggs=False)
