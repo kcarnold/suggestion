8c27cc6d7d68d3813481231534228f954d522b14
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-04-19 14:13:08 -0400

    Add fabfile for deploy

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import local, lcd, env, cd, run
+from fabric.contrib.project import rsync_project
+
+env.use_ssh_config = True
+env.hosts = ['megacomplete-aws']
+
+def deploy():
+    with cd('~/code/suggestion'):
+        run('git pull')
+    with lcd('frontend'):
+        local('npm run build')
+    rsync_project(remote_dir='~/code/suggestion/frontend/build/', local_dir='frontend/build/', delete=True)
