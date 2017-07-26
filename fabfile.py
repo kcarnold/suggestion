d0158233bca7b469e92015628e9d7b860c51415e
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-07-26 18:32:19 -0400

    Sync models before the server restarts

--- fabfile.py
+++ fabfile.py
@@ -13,13 +13,13 @@ def deploy():
     local('git push')
     git_rev = subprocess.check_output(['git', 'describe', '--always']).decode('utf-8').strip()
     open('frontend/.env', 'w').write(f'REACT_APP_GIT_REV={git_rev}\n')
+    rsync_project(remote_dir='~/code/suggestion/models/', local_dir='models/', delete=True)
     with cd('~/code/suggestion'):
         run('git pull')
     with lcd('frontend'):
         local('npm run build')
     rsync_project(remote_dir='~/code/suggestion/frontend/build/', local_dir='frontend/build/', delete=True)
     # rsync -Pax models/ megacomplete-aws:/home/ubuntu/code/suggestion/models/
-    rsync_project(remote_dir='~/code/suggestion/models/', local_dir='models/', delete=True)
     with lcd('frontend'):
         local(f'sentry-cli releases -o kenneth-arnold -p suggestionfrontend new {git_rev}')
         local(f'sentry-cli releases -o kenneth-arnold -p suggestionfrontend files {git_rev} upload-sourcemaps src build')
