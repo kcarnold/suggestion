23df8ee9edc29bd9151668ec08a6bebdab71c948
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-08-02 10:40:30 -0400

    Run prettier on test code.

--- frontend/src/ExperimentState.test.js
+++ frontend/src/ExperimentState.test.js
@@ -1,22 +1,31 @@
-import {ExperimentStateStore} from './ExperimentState';
+import { ExperimentStateStore } from "./ExperimentState";
 
 const recs0 = {
-  "predictions": [{"word": "of"}, {"word": "and"}, {"word": "the"}], "replacement_range": [4, 10], "synonyms": [{"word": "front"}, {"word": "interior"}, {"word": "exterior"}]
+  predictions: [{ word: "of" }, { word: "and" }, { word: "the" }],
+  replacement_range: [4, 10],
+  synonyms: [{ word: "front" }, { word: "interior" }, { word: "exterior" }],
 };
 
-it('inserts automatic spaces after suggestions', () => {
+it("inserts automatic spaces after suggestions", () => {
   var state = new ExperimentStateStore({});
   const curText = "the inside ";
-  Array.prototype.forEach.call(curText, key => state.handleEvent({type: 'tapKey', key}));
+  Array.prototype.forEach.call(curText, key =>
+    state.handleEvent({ type: "tapKey", key }),
+  );
   expect(state.curText).toEqual(curText);
 
-  state.handleEvent({type: 'receivedSuggestions', msg: {request_id: state.contextSequenceNum, ...recs0}});
-  expect(state.visibleSuggestions.replacement_range).toEqual(recs0.replacement_range);
+  state.handleEvent({
+    type: "receivedSuggestions",
+    msg: { request_id: state.contextSequenceNum, ...recs0 },
+  });
+  expect(state.visibleSuggestions.replacement_range).toEqual(
+    recs0.replacement_range,
+  );
   expect(state.visibleSuggestions.synonyms).toEqual(recs0.synonyms);
 
-  state.handleEvent({type: 'tapSuggestion', which: 'synonyms', slot: 1});
+  state.handleEvent({ type: "tapSuggestion", which: "synonyms", slot: 1 });
   expect(state.curText).toEqual("the interior ");
 
-  state.handleEvent({type: 'tapKey', key: '.'});
+  state.handleEvent({ type: "tapKey", key: "." });
   expect(state.curText).toEqual("the interior. ");
 });
--- frontend/src/MasterStateStore.test.js
+++ frontend/src/MasterStateStore.test.js
@@ -1,27 +1,34 @@
-import Promise from 'bluebird';
+import Promise from "bluebird";
 const readFile = Promise.promisify(require("fs").readFile);
-import {MasterStateStore} from './MasterStateStore';
+import { MasterStateStore } from "./MasterStateStore";
 
-const participantIds = ['smoke0'];
+const participantIds = ["smoke0"];
 let logData = {};
 
 function readLogFile(participantId) {
   let filename = `${participantId}.jsonl`;
-  if (!participantId.slice(0, 5) === 'smoke') filename = '../logs/' + filename;
-  return readFile(filename, 'utf8')
-      .then(data => [participantId, data.split('\n').filter(line => line.length > 0).map(line => JSON.parse(line))]);
+  if (!participantId.slice(0, 5) === "smoke") filename = "../logs/" + filename;
+  return readFile(filename, "utf8").then(data => [
+    participantId,
+    data
+      .split("\n")
+      .filter(line => line.length > 0)
+      .map(line => JSON.parse(line)),
+  ]);
 }
 
 beforeAll(() => {
-  return Promise.map(participantIds, readLogFile).then(logs => {
-    console.log(`Loaded ${logs.length} logs.`);
-    logData = logs;
-  }).catch(err => console.error(err));
-})
+  return Promise.map(participantIds, readLogFile)
+    .then(logs => {
+      console.log(`Loaded ${logs.length} logs.`);
+      logData = logs;
+    })
+    .catch(err => console.error(err));
+});
 
-it('creates state without crashing', () => {
+it("creates state without crashing", () => {
   logData.forEach(([participantId, log]) => {
     var state = new MasterStateStore(participantId);
     log.forEach(entry => state.handleEvent(entry));
-  })
+  });
 });
