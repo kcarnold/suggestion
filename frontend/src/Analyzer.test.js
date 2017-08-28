3684912dd473b107ca150faecda18b875e80462a
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-08-28 17:55:12 -0400

    Remove outdated code

--- frontend/src/Analyzer.test.js
+++ frontend/src/Analyzer.test.js
@@ -9,22 +9,6 @@ const participantIds = [
 let logData = null;
 let analyzed = null;
 
-function updateLog(log) {
-  // Port a log to the new format.
-  return log.map(entry => {
-    if (false || entry.type === 'receivedSuggestions') {
-      let msg = {...entry.msg};
-      ['predictions', 'synonyms'].forEach(typ => {
-        if (msg[typ]) {
-          msg[typ] = msg[typ].map(ent => ({words: [ent.word]}));
-        }
-      });
-      entry = {...entry, msg};
-    }
-    return entry;
-  })
-}
-
 beforeAll(async () => {
   let logs = await Promise.map(participantIds, readLogFile);
   console.log(`Loaded ${logs.length} logs.`);
