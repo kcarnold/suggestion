ff778afd3679f02dde93e42d641434d005422143
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-06 09:45:52 -0400

    Make sure each new page scrolls to top.

--- frontend/src/MasterView.js
+++ frontend/src/MasterView.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {observer, inject} from 'mobx-react';
 import * as Views from './Views';
 
 
-export const MasterView = inject('state')(observer(({state, kind}) => {
-  if (state.replaying) return <div>Loading...</div>;
-  let screenDesc = state.screens[state.screenNum];
-  let screenName;
-  if (kind === 'c') {
-    screenName = screenDesc.controllerScreen || 'LookAtPhone';
-  } else {
-    screenName = screenDesc.screen || 'LookAtComputer';
+export const MasterView = inject('state')(observer(class MasterView extends Component {
+  componentDidUpdate() {
+    window.scrollTo(0, 0);
   }
-  return (
-    <div className="App">
-      {React.createElement(Views[screenName])}
-    </div>);
+
+  render() {
+    let {state, kind} = this.props;
+    if (state.replaying) return <div>Loading...</div>;
+    let screenDesc = state.screens[state.screenNum];
+    let screenName;
+    if (kind === 'c') {
+      screenName = screenDesc.controllerScreen || 'LookAtPhone';
+    } else {
+      screenName = screenDesc.screen || 'LookAtComputer';
+    }
+    return (
+      <div className="App">
+        {React.createElement(Views[screenName])}
+      </div>);
+  }
 }));
 
 export default MasterView;
