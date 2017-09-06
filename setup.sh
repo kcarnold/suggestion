de0d38c457576f2f2c51f570e475fe3ad22ee4ea
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-06 15:12:16 -0400

    Update setup stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 conda install -y tornado ujson cython numpy datrie nltk scipy numba scikit-learn cytoolz pandas
 pip install -r requirements.txt
+pip install -e .
 
 # Dev install:
 # conda install -y fabric3 tqdm
