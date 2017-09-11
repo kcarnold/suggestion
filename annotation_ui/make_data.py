f6ec95d3068d386825eb795b3c7fdffacab522ca
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-11 16:36:52 -0400

    Add more participant completions

--- annotation_ui/make_data.py
+++ annotation_ui/make_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 import toolz
 todos = json.load(open('../gruntwork/sent4_1_annotations_todo.json'))
 BATCH_SIZE=8
+print("Total:", len(todos))
 batches = list(toolz.partition_all(BATCH_SIZE, todos))
 print(pd.Series([len(batch) for batch in batches]).value_counts())
 pd.DataFrame(dict(task=[json.dumps(batch) for batch in batches])).to_csv('task.csv', index=False)
