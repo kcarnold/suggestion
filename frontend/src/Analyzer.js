e7205d8bec727f4e299657beb01ecc414bba2c94
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-11 23:22:27 -0400

    Prettier Analyzer.js

--- frontend/src/Analyzer.js
+++ frontend/src/Analyzer.js
@@ -1,14 +1,14 @@
-import * as M from 'mobx';
-import _ from 'lodash';
+import * as M from "mobx";
+import _ from "lodash";
 
-let HACK_TSCODES_TO_SKIP = {}
+let HACK_TSCODES_TO_SKIP = {};
 const CODES = "p964wg-1504799690416";
 CODES.split(/\s/).forEach(code => {
   HACK_TSCODES_TO_SKIP[code] = true;
 });
 
 export function processLogGivenStateStore(StateStoreClass, log) {
-  let {participant_id} = log[0];
+  let { participant_id } = log[0];
   let state = new StateStoreClass(participant_id);
   let byExpPage = {};
   let pageSeq = [];
@@ -21,7 +21,7 @@ export function processLogGivenStateStore(StateStoreClass, log) {
         displayedSuggs: [],
         condition: state.conditionName,
         place: state.curPlace,
-        finalText: '',
+        finalText: "",
         actions: [],
         firstEventTimestamp: null,
         lastEventTimestamp: null,
@@ -38,7 +38,6 @@ export function processLogGivenStateStore(StateStoreClass, log) {
   let stateMismatches = [];
 
   log.forEach((entry, logIdx) => {
-
     // We need to track context sequence numbers instead of curText because
     // autospacing after punctuation seems to increment contextSequenceNum
     // without changing curText.
@@ -46,23 +45,31 @@ export function processLogGivenStateStore(StateStoreClass, log) {
     let lastText = (state.experimentState || {}).curText;
     let lastDisplayedSuggs = null;
 
-    let isValidSugUpdate = entry.type === 'receivedSuggestions' && entry.msg.request_id === (state.experimentState || {}).contextSequenceNum;
+    let isValidSugUpdate =
+      entry.type === "receivedSuggestions" &&
+      entry.msg.request_id === (state.experimentState || {}).contextSequenceNum;
 
     // Track requests
-    if (entry.kind === 'meta' && entry.type === 'requestSuggestions') {
+    if (entry.kind === "meta" && entry.type === "requestSuggestions") {
       let msg = _.clone(entry.request);
-      let requestCurText = msg.sofar + msg.cur_word.map((ent => ent.letter)).join('');
-      requestsByTimestamp[msg.timestamp] = {request: msg, response: null};
+      let requestCurText =
+        msg.sofar + msg.cur_word.map(ent => ent.letter).join("");
+      requestsByTimestamp[msg.timestamp] = { request: msg, response: null };
       if (tmpSugRequests[msg.request_id]) {
-        console.assert(tmpSugRequests[msg.request_id] === requestCurText, `Mismatch request curText for ${participant_id}-${msg.timestamp}}, "${tmpSugRequests[msg.request_id]}" VS "${requestCurText}"`);
+        console.assert(
+          tmpSugRequests[msg.request_id] === requestCurText,
+          `Mismatch request curText for ${participant_id}-${msg.timestamp}}, "${tmpSugRequests[
+            msg.request_id
+          ]}" VS "${requestCurText}"`,
+        );
         // console.log("Ignoring duplicate request", msg.timestamp);
         requestsByTimestamp[msg.timestamp].dupe = true;
         return;
       } else {
         tmpSugRequests[msg.request_id] = requestCurText;
       }
-    } else if (entry.type === 'receivedSuggestions') {
-      let msg = {...entry.msg, responseTimestamp: entry.jsTimestamp};
+    } else if (entry.type === "receivedSuggestions") {
+      let msg = { ...entry.msg, responseTimestamp: entry.jsTimestamp };
       let tscode = `${participant_id}-${msg.timestamp}`;
       if (HACK_TSCODES_TO_SKIP[tscode]) {
         return;
@@ -71,8 +78,7 @@ export function processLogGivenStateStore(StateStoreClass, log) {
       requestsByTimestamp[msg.timestamp].response = msg;
     }
 
-
-    if (entry.kind !== 'meta') {
+    if (entry.kind !== "meta") {
       // if (entry.type !== 'receivedSuggestions' || isValidSugUpdate)
       state.handleEvent(entry);
     }
@@ -95,18 +101,42 @@ export function processLogGivenStateStore(StateStoreClass, log) {
     pageData.lastEventTimestamp = entry.jsTimestamp;
 
     // Assert state consistency
-    if (entry.kind === 'meta' && entry.type === 'requestSuggestions' && entry.request.request_id === expState.contextSequenceNum) {
-      let requestCurText = entry.request.sofar + entry.request.cur_word.map((ent => ent.letter)).join('');
+    if (
+      entry.kind === "meta" &&
+      entry.type === "requestSuggestions" &&
+      entry.request.request_id === expState.contextSequenceNum
+    ) {
+      let requestCurText =
+        entry.request.sofar +
+        entry.request.cur_word.map(ent => ent.letter).join("");
       if (requestCurText !== expState.curText) {
         stateMismatches.push(lastSugResponseTimestamp);
-        console.log(participant_id, logIdx, "Correcting curText:", expState.curText, 'TO', requestCurText)
+        console.log(
+          participant_id,
+          logIdx,
+          "Correcting curText:",
+          expState.curText,
+          "TO",
+          requestCurText,
+        );
         expState.curText = requestCurText;
         // throw new Error(`State mismatch! ${entry.request.sofar} vs ${expState.suggestionContext.prefix} - last sug response ${lastSugResponseTimestamp}`)
       }
     }
 
-    if (['connected', 'init', 'requestSuggestions', 'receivedSuggestions'].indexOf(entry.type) === -1) {
-      pageData.actions.push({...entry, curText: lastText, timestamp: entry.jsTimestamp});
+    if (
+      [
+        "connected",
+        "init",
+        "requestSuggestions",
+        "receivedSuggestions",
+      ].indexOf(entry.type) === -1
+    ) {
+      pageData.actions.push({
+        ...entry,
+        curText: lastText,
+        timestamp: entry.jsTimestamp,
+      });
     }
 
     let visibleSuggestions = M.toJS(expState.visibleSuggestions);
@@ -115,8 +145,8 @@ export function processLogGivenStateStore(StateStoreClass, log) {
         pageData.displayedSuggs[lastContextSeqNum].action = entry;
       }
       lastContextSeqNum = expState.contextSequenceNum;
-    } else if (entry.type === 'receivedSuggestions' && isValidSugUpdate) {
-      let {request, response}  = requestsByTimestamp[entry.msg.timestamp];
+    } else if (entry.type === "receivedSuggestions" && isValidSugUpdate) {
+      let { request, response } = requestsByTimestamp[entry.msg.timestamp];
       pageData.displayedSuggs[expState.contextSequenceNum] = {
         request_id: request.request_id,
         sofar: request.sofar,
@@ -130,8 +160,13 @@ export function processLogGivenStateStore(StateStoreClass, log) {
       };
     }
 
-    if (pageData.displayedSuggs[expState.contextSequenceNum] && !_.isEqual(visibleSuggestions, lastDisplayedSuggs)) {
-      pageData.displayedSuggs[expState.contextSequenceNum].recs = visibleSuggestions;
+    if (
+      pageData.displayedSuggs[expState.contextSequenceNum] &&
+      !_.isEqual(visibleSuggestions, lastDisplayedSuggs)
+    ) {
+      pageData.displayedSuggs[
+        expState.contextSequenceNum
+      ].recs = visibleSuggestions;
       lastDisplayedSuggs = visibleSuggestions;
     }
   });
@@ -141,22 +176,31 @@ export function processLogGivenStateStore(StateStoreClass, log) {
     let pageData = byExpPage[pageName];
     let expState = state.experiments.get(pageName);
     pageData.finalText = expState.curText;
-    pageData.displayedSuggs[pageData.displayedSuggs.length - 1].action = {type: 'next'};
-    pageData.secsOnPage = (pageData.lastEventTimestamp - pageData.firstEventTimestamp) / 1000;
+    pageData.displayedSuggs[pageData.displayedSuggs.length - 1].action = {
+      type: "next",
+    };
+    pageData.secsOnPage =
+      (pageData.lastEventTimestamp - pageData.firstEventTimestamp) / 1000;
   });
 
   if (stateMismatches.length) {
-    console.error(stateMismatches.join(' '));
-    throw new Error(`State mismatches: ${stateMismatches.join(' ')}`);
+    console.error(stateMismatches.join(" "));
+    throw new Error(`State mismatches: ${stateMismatches.join(" ")}`);
   }
 
   // One log didn't get to the last
-  console.assert(state.curScreen.screen === 'Done' || state.curScreen.screen === 'IntroSurvey', "Incomplete log file %s", participant_id);
+  console.assert(
+    state.curScreen.screen === "Done" ||
+      state.curScreen.screen === "IntroSurvey",
+    "Incomplete log file %s",
+    participant_id,
+  );
 
   let screenTimes = state.screenTimes.map(screen => {
     let screenDesc = state.screens[screen.num];
     return {
-      ...screen, name: screenDesc.screen || screenDesc.controllerScreen
+      ...screen,
+      name: screenDesc.screen || screenDesc.controllerScreen,
     };
   });
 
@@ -171,8 +215,9 @@ export function processLogGivenStateStore(StateStoreClass, log) {
 }
 
 async function getStateStoreClass(log) {
-  let {rev} = log[0];
-  return (await import(`../../old-code/${rev}/frontend/src/MasterStateStore`)).MasterStateStore;
+  let { rev } = log[0];
+  return (await import(`../../old-code/${rev}/frontend/src/MasterStateStore`))
+    .MasterStateStore;
 }
 
 export async function analyzeLog(log) {
