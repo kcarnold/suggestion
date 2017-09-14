f91ce5e384c9523241c89f8932812c49917c1a22
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-14 10:40:59 -0400

    Randomized annotation task as posted.

--- annotation_ui/make_comparison_task.py
+++ annotation_ui/make_comparison_task.py
@@ -3,7 +3,11 @@ import json
 import pandas as pd
 import toolz
 todos = json.load(open('../gruntwork/comparisons_existing_reviews.json'))
-BATCH_SIZE=10
+
+import random
+random.Random(0).shuffle(todos)
+
+BATCH_SIZE=20
 print("Total:", len(todos))
 batches = list(toolz.partition_all(BATCH_SIZE, todos))
 print(pd.Series([len(batch) for batch in batches]).value_counts())
