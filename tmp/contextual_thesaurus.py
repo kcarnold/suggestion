d8c63bbe59a73c57e364c991dc1e64bb19b7b7e3
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-26 14:27:58 -0400

    WIP experiments with contextual alternatives

--- tmp/contextual_thesaurus.py
+++ tmp/contextual_thesaurus.py
@@ -88,3 +88,66 @@ for sofar in ['the service was ', 'the food was ', 'i loved the ', 'i ', 'the ',
 
     print()
     print()
+
+
+
+#%%
+# Let's try to get a diverse set of word vecs. What's diverse? "marvelous, wonderful, fantastic" isn't. "cute", "cozy", "nice" is...
+example = 'i loved the food'
+sofar, query_word = example.rsplit(' ', 1)
+sofar = sofar + ' '
+word_vecs = suggestion_generator.word_vecs_for_model['yelp_train-balanced']
+#query_word = 'great'
+query_word_idx = model.model.vocab_index(query_word)
+query_word_vec = word_vecs[query_word_idx]
+sim_to_query = pairwise.cosine_similarity([query_word_vec], word_vecs)[0]
+print(' '.join(model.id2str[idx] for idx in np.argsort(sim_to_query)[-10:]))
+#% amazing superb awesome good tremendous excellent fantastic wonderful terrific great
+#%% So those are too "same". Let's find a threshold of similarity in which a random sample still gets us reasonably similar.
+similarity_threshold = 0.25
+rs = np.random.RandomState(0)
+orig_llk = model.unigram_probs[query_word_idx]
+max_likelihood = orig_llk + 0
+min_likelihood = orig_llk - 3.
+criteria = (
+        (sim_to_query > similarity_threshold) &
+        (model.unigram_probs > min_likelihood) &
+        (model.unigram_probs < max_likelihood))
+passes_criteria = np.flatnonzero(criteria)
+print(' '.join(model.id2str[idx] for idx in rs.choice(passes_criteria, 50, replace=False)))
+print(len(passes_criteria))
+
+toks = suggestion_generator.tokenize_sofar(sofar)
+state = model.get_state(toks)[0]
+contextual_logprobs = model.eval_logprobs_for_words(state, passes_criteria)
+
+orig_contextual_logprob = model.eval_logprobs_for_words(state, [query_word_idx])[0]
+passes_new_criteria_newidx = np.flatnonzero((contextual_logprobs > -10))
+passes_new_criteria = passes_criteria[passes_new_criteria_newidx]
+print(' '.join(model.id2str[idx] for idx in passes_new_criteria))
+#%%
+vecs_for_clustering = word_vecs[passes_new_criteria]
+n_clusters = 5
+kmeans = KMeans(n_clusters=n_clusters, max_iter=100, n_init=10).fit(vecs_for_clustering)
+cluster_assignment = kmeans.predict(vecs_for_clustering)
+print("Next-word clusters:")
+relevance = contextual_logprobs[passes_new_criteria_newidx]
+for cluster in range(n_clusters):
+    members = np.flatnonzero(cluster_assignment == cluster)
+    relevances = relevance[members]
+    new_order = np.argsort(relevances)[::-1][:10]
+    members = members[new_order]
+    relevances = relevance[members]
+    print(cluster, ', '.join('{}[{:.2f}]'.format(model.id2str[passes_new_criteria[idx]], relevance[idx]) for idx in members))
+
+
+#%%
+print(orig_contextual_logprob, orig_llk)
+for word in 'sweet tasty filling colorful authentic yucky tasteless bland slimy apartment shape ann '.split():
+    print(f'\n{word}')
+    word_idx = model.model.vocab_index(word)
+    print(f'- sim: {sim_to_query[word_idx]:.2f}')
+    print(f'- unillk: {model.unigram_probs[word_idx]}')
+    print(f'- contextllk: {model.eval_logprobs_for_words(state, [word_idx])[0]:.2f}')
+
+    #%%
