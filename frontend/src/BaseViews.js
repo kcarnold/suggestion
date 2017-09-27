77fd6732b2da246b2eb3afbecd10ebd87ac83109
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-09-27 14:55:52 -0400

    Minor tweaks and prettier on BaseViews

--- frontend/src/BaseViews.js
+++ frontend/src/BaseViews.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
-import {observer, inject} from 'mobx-react';
+import React from "react";
+import { observer, inject } from "mobx-react";
 
 function advance(state, dispatch) {
-  dispatch({type: 'next'})
+  dispatch({ type: "next" });
 }
 
-export const NextBtn = inject('dispatch', 'state')((props) => <button onClick={() => {
-  if (!props.confirm || window.confirm("Are you sure?")) {
-    advance(props.state, props.dispatch);
-  }
-  }} disabled={props.disabled}>{props.children || "Next"}</button>);
-
+export const NextBtn = inject("dispatch", "state")(
+  observer(props =>
+    <button
+      onClick={() => {
+        if (!props.confirm || window.confirm("Are you sure?")) {
+          advance(props.state, props.dispatch);
+        }
+      }}
+      disabled={props.disabled}
+    >
+      {props.children || "Next"}
+    </button>,
+  ),
+);
