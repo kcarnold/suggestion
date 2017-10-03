8e203bcc5f95ff87ff3e85af599f4774596fe272
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-10-03 18:43:34 -0400

    Push out a sentiment annotation for this batch. Repeat a few to make it divide evenly.

--- annotation_ui/make_data.py
+++ annotation_ui/make_data.py
@@ -2,9 +2,14 @@
 import json
 import pandas as pd
 import toolz
-todos = json.load(open('../gruntwork/sent4_1_annotations_todo.json'))
+todos = json.load(open('../gruntwork/persuade_0_annotations_todo.json'))
 BATCH_SIZE=4
 print("Total:", len(todos))
 batches = list(toolz.partition_all(BATCH_SIZE, todos))
 print(pd.Series([len(batch) for batch in batches]).value_counts())
+if len(batches[-1]) != BATCH_SIZE:
+    print("Tacking on extra to the last batch.")
+    batches[-1] = (batches[-1] + batches[0])[:BATCH_SIZE]
+assert len(batches[-1]) == BATCH_SIZE
 pd.DataFrame(dict(task=[json.dumps(batch) for batch in batches])).to_csv('anno-task.csv', index=False)
+
