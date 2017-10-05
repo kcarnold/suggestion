b2e0f3a6b2737d5a48cd516d3df25d89dd014ec4
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2017-10-05 16:32:53 -0400

    Pairwise persuasiveness ratings

--- annotation_ui/build.py
+++ annotation_ui/build.py
@@ -2,14 +2,29 @@
 
 from jinja2 import Template
 import pandas as pd
+import hashlib
+
+prod_basepath = 'https://s3.amazonaws.com/megacomplete.net/anno/'
 
 def build_hit(basename):
-    example_task = pd.read_csv(f'{basename}-task.csv').task.iloc[0]
-    t = Template(open(f'{basename}.html').read())
-    open(f'{basename}-dev.html', 'w').write(t.render(dev=True, task=example_task))
-    open(f'{basename}-prod.html', 'w').write(t.render(dev=False))
+    for config in ['dev', 'prod']:
+        args = {}
+        if config == 'dev':
+            example_task = pd.read_csv(f'{basename}-task.csv').task.iloc[0]
+            t = Template(open(f'{basename}.html').read())
+            args['task'] = example_task
+            args['dev'] = True
+            basepath = ''
+        else:
+            basepath = prod_basepath
+        for resource in ['css', 'js']:
+            filename = f'{basename}.{resource}'
+            filehash = hashlib.sha1(open(filename, 'rb').read()).hexdigest()
+            args[resource] = f'{basepath}{filename}?{filehash}'
+        open(f'{basename}-{config}.html', 'w').write(t.render(**args))
 
-build_hit('anno')
-build_hit('compare-anno')
-build_hit('persuasive')
-build_hit('avoid')
+# build_hit('anno')
+# build_hit('compare-anno')
+# build_hit('persuasive')
+# build_hit('avoid')
+build_hit('pairwise-persuasive')
---
+++ annotation_ui/pairwise-persuasive.js
@@ -0,0 +1,23 @@
+var procData = _.map(taskData, function(pair, pairIdx) {
+  let swap = Math.random() < 0.5;
+  let texts = _.map(pair.texts, function(text, i) {
+    return _.assign({ idx: i }, text);
+  });
+  if (swap) {
+    texts = [texts[1], texts[0]];
+  }
+  return {
+    pairIdx: pairIdx,
+    meta: pair.meta,
+    swap: swap,
+    texts: texts,
+    selected: null,
+  };
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    pairs: procData,
+  },
+});
