27650cbf724b77361f8f4e609774ecb9bcf9e3c9
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2018-12-21 18:02:31 -0500

    Hack setup.py so Poetry doesn't crash

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup, Extension
-from Cython.Build import cythonize
+from setuptools import setup, Extension
+#from Cython.Build import cythonize
 import numpy as np
 
-extensions = [
-    Extension('suggestion.suffix_sort', ['suggestion/qsufsort.c', 'suggestion/suffix_sort.pyx'], include_dirs=[np.get_include()])
-]
+#extensions = [
+#    Extension('suggestion.suffix_sort', ['suggestion/qsufsort.c', 'suggestion/suffix_sort.pyx'], include_dirs=[np.get_include()])
+#]
 
 setup(
     name="suggestion",
     packages=['suggestion'],
-    ext_modules=cythonize(extensions)
+#    ext_modules=cythonize(extensions),
+    version="0.0.1",
+    description="Old code for text suggestion.",
+    install_requires=["nltk"],
 )
